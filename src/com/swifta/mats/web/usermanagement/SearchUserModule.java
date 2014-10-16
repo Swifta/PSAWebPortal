4c28ce0fb22b2cfd25ac39926537ce86ceac82ee
Livepwndz <pwndz172@gmail.com>
2014-10-16 18:00:57 +0300

    Customising Search results for all User Types

--- src/com/swifta/mats/web/usermanagement/SearchUserModule.java
+++ src/com/swifta/mats/web/usermanagement/SearchUserModule.java
@@ -1,7 +1,6 @@
 package com.swifta.mats.web.usermanagement;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import com.swifta.mats.web.WorkSpace;
@@ -14,18 +13,18 @@ import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.CheckBox;
+import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.Embedded;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextArea;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.themes.Reindeer;
-import com.vaadin.ui.themes.ValoTheme;
 
 public class SearchUserModule {
 	public static  final String tbUsers = "user";
@@ -40,12 +39,14 @@ public class SearchUserModule {
 	
 	public static final String SESSION_SEARCH_USER = "search_user";
 	public static final String SESSION_VAR_SEARCH_USER_DEFAULT = "agent";
+	public static final String SESSION_SEARCH_USER_PARAM = "search_user_param";
 	/*public static final String SESSION_VAR_SEARCH_USER = "merchant";
 	public static final String SESSION_VAR_SEARCH_USER = "agent";
 	public static final String SESSION_VAR_SEARCH_USER = "agent";
 	public static final String SESSION_VAR_SEARCH_USER = "agent";
 	public static final String SESSION_VAR_SEARCH_USER = "agent";
 	public static final String SESSION_VAR_SEARCH_USER = "agent";*/
+	
 	
 	
 	BtnActions btnDetails;
@@ -87,8 +88,9 @@ public class SearchUserModule {
 	}
 	
 	@SuppressWarnings("unchecked")
-	public IndexedContainer queryBackEnd(){
-		IndexedContainer container = new IndexedContainer();//"Results for: \""+UI.getCurrent().getSession().getAttribute(ManageUserModule.UMANAGE_SESSION_SEARCH)+"\"  (Summary)");
+	private IndexedContainer queryBackEnd(String strSearchParams){
+		IndexedContainer container = new IndexedContainer();	
+	
 		container.addContainerProperty(" ", CheckBox.class, null);
 		container.addContainerProperty("UID", String.class, "000");
 		container.addContainerProperty("Username", String.class, "");
@@ -132,7 +134,7 @@ public class SearchUserModule {
 		actionsC.addComponent(btnLink);
 		actionsC.addComponent(btnDelete);
 		actionsC.addComponent(btnMoreActions);
-	
+		
 		
 		
 		
@@ -733,6 +735,8 @@ public class SearchUserModule {
 		searchUserHeader.addComponent(lbSearch);
 		searchUserHeader.setStyleName("search_user_header");
 		
+	    
+		
 		ArrayList<String> arrLTfCaptions = new ArrayList<String>();
 		arrLTfCaptions.add(strUserType+" ID");
 		arrLTfCaptions.add("Username");
@@ -743,7 +747,7 @@ public class SearchUserModule {
 		arrLTfCaptions.add("Others");
 		
 		searchForm.addComponent(searchUserHeader);
-		addTfs(arrLTfCaptions, searchForm);
+		final ArrayList<TextField> arrLTfs = addTfs(arrLTfCaptions, searchForm);
 		Button btnSearch = new Button("Search");
 		searchForm.addComponent(btnSearch);
 		
@@ -757,9 +761,21 @@ public class SearchUserModule {
 
 			@Override
 			public void buttonClick(ClickEvent event) {
-				UI.getCurrent().getSession().setAttribute(ManageUserModule.SESSION_UMANAGE, ManageUserModule.SESSION_VAR_UMANAGE_SEARCH_RESULTS);
-				UI.getCurrent().getSession().setAttribute(ManageUserModule.SESSION_UMANAGE, ManageUserModule.SESSION_VAR_UMANAGE_SEARCH_RESULTS);
+				StringBuilder strBuilder = new StringBuilder();
+				for(TextField tF: arrLTfs){
+					
+					strBuilder.append(tF.getCaption());
+					strBuilder.append("=");
+					strBuilder.append(tF.getValue());
+					strBuilder.append("&");
+				}
 				
+				String strParams = strBuilder.toString();
+				
+				
+				UI.getCurrent().getSession().setAttribute(ManageUserModule.SESSION_UMANAGE, ManageUserModule.SESSION_VAR_UMANAGE_SEARCH_RESULTS);
+				UI.getCurrent().getSession().setAttribute(SESSION_SEARCH_USER_PARAM, strParams);
+
 				if(WorkSpace.wsmu != null)
 					WorkSpace.wsmu.wsmuModifier();
 			}
@@ -773,14 +789,120 @@ return searchForm;
 }
 	
 	
+
+public VerticalLayout getSearchResults(String strSearchParams){
+	VerticalLayout searchResultsContainer = new VerticalLayout();
+	searchResultsContainer.setSizeUndefined();
+	searchResultsContainer.setSpacing(true);
+	searchResultsContainer.setMargin(new MarginInfo(false, true, true, true));
 	
-private void addTfs(ArrayList<String> arrLTfCaptions, FormLayout searchForm){
+	String[] arrAllParams = strSearchParams.split("&");
+	
+	StringBuilder strBuilder = new StringBuilder();
+	
+	
+	String[] arrP = null;
+	for(String strParam: arrAllParams){
+		arrP = strParam.split("=");
+		if(arrP.length == 2){
+			strBuilder.append(arrP[0]);
+			strBuilder.append(" of ");
+			strBuilder.append(arrP[1]);
+			strBuilder.append(", ");
+		}
+	}
+	
+	
+	int iLastIndexOfComma = strBuilder.lastIndexOf(",");
+	if(iLastIndexOfComma != -1){
+		strBuilder.delete(iLastIndexOfComma, iLastIndexOfComma+1);
+	}
+	String strSearchResultsParams = null;
+	iLastIndexOfComma = strBuilder.lastIndexOf(",");
+	if(iLastIndexOfComma != -1){
+		strBuilder.replace(iLastIndexOfComma, iLastIndexOfComma+1, " and");
+		strSearchResultsParams = strBuilder.toString();
+	}else{
+		strSearchResultsParams = strBuilder.toString();
+	}
+	//Notification.show(Integer.toString(strSearchResultsParams.length()));
+	strSearchResultsParams = strBuilder.toString();
+	if(strSearchResultsParams.length()== 0){
+		strSearchResultsParams = "Nothing.";
+	}
+	
+	
+	Label lbSearch = new Label("Match results for: "+strSearchResultsParams);
+	lbSearch.setSizeUndefined();
+	lbSearch.setStyleName("label_search_user");
+	lbSearch.setSizeUndefined();
+	
+	VerticalLayout searchUserHeader = new VerticalLayout();
+	searchUserHeader.setHeightUndefined();
+	searchUserHeader.setWidthUndefined();
+	searchUserHeader.setMargin(false);
+	searchUserHeader.setSpacing(true);
+	//searchUserHeader.addComponent(emb);
+	searchUserHeader.addComponent(lbSearch);
+	searchUserHeader.setStyleName("search_results_header");
+	searchResultsContainer.addComponent(searchUserHeader);
+	
+	
+	IndexedContainer container = queryBackEnd(strSearchParams);
+	
+	PagedTableCustom tb = new PagedTableCustom("Results (Summary)");
+	tb.setContainerDataSource(container);
+	tb.setColumnIcon(" ", FontAwesome.CHECK_SQUARE_O);
+	tb.setStyleName("tb_u_search_results");
+	
+	HorizontalLayout pnUserSearchResults = tb.createControls();
+	pnUserSearchResults.setSizeFull();
+	pnUserSearchResults.setMargin(false);
+	pnUserSearchResults.setSpacing(false);
+	
+	searchResultsContainer.addComponent(pnUserSearchResults);
+	searchResultsContainer.addComponent(tb);
+	
+	VerticalLayout actionBulkC = new VerticalLayout();
+	actionBulkC.setWidth("100%");
+	actionBulkC.setStyleName("c_action_bulk");
+	
+	CheckBox chkAll = new CheckBox();
+	chkAll.setCaption("Select All");
+	
+	ComboBox cmbBulk = new ComboBox("Bulk Action");
+	cmbBulk.addItem("Delete");
+	cmbBulk.addItem("Link");
+	
+	Button btnBulkOk = new Button("Apply");
+	btnBulkOk.setStyleName("btn_link");
+	HorizontalLayout cBulk = new HorizontalLayout();
+	cBulk.setSizeUndefined();
+	cBulk.addComponent(cmbBulk);
+	cBulk.addComponent(btnBulkOk);
+	cBulk.setComponentAlignment(btnBulkOk, Alignment.BOTTOM_RIGHT);
+	
+	actionBulkC.addComponent(chkAll);
+	actionBulkC.addComponent(cBulk);
+	
+	searchResultsContainer.addComponent(actionBulkC);
+	return searchResultsContainer;
+}
+
+	
+	
+	
+private ArrayList<TextField>addTfs(ArrayList<String> arrLTfCaptions, FormLayout searchForm){
 
 	TextField tF;
+	ArrayList<TextField> arrLTfs = new ArrayList<TextField>();
 	for(String tFCaption: arrLTfCaptions){
 			tF = new TextField(tFCaption);
+			arrLTfs.add(tF);
+			
 			searchForm.addComponent(tF);
 	}
+	return arrLTfs;
 }
 
 	
