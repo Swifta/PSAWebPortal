9188e0fba696e6c168a0b463e3d816ed918fd8fd
kadeniyi <kadeniyi@swifta.com>
2014-11-11 16:10:49 +0100

    UserManager Modified

--- src/com/swifta/mats/web/utils/UserManagementService.java
+++ src/com/swifta/mats/web/utils/UserManagementService.java
@@ -1,81 +1,67 @@
 package com.swifta.mats.web.utils;
 
 import java.rmi.RemoteException;
-import java.util.Calendar;
-import java.util.Date;
-
-import org.apache.axis2.AxisFault;
 
 import com.swifta.sub.mats.operation.provisioning.v1_0.ProvisioningStub;
 import com.swifta.sub.mats.operation.provisioning.v1_0.ProvisioningStub.Accountholderdetails;
-import com.swifta.sub.mats.operation.provisioning.v1_0.ProvisioningStub.Address;
-import com.swifta.sub.mats.operation.provisioning.v1_0.ProvisioningStub.Identification;
-import com.swifta.sub.mats.operation.provisioning.v1_0.ProvisioningStub.Operationresponse;
-import com.swifta.sub.mats.operation.provisioning.v1_0.ProvisioningStub.PrimaryContactInfo;
 import com.swifta.sub.mats.operation.provisioning.v1_0.ProvisioningStub.Registration;
 import com.swifta.sub.mats.operation.provisioning.v1_0.ProvisioningStub.RegistrationE;
+import com.swifta.sub.mats.operation.provisioning.v1_0.ProvisioningStub.RegistrationResponse;
+import com.swifta.sub.mats.operation.provisioning.v1_0.ProvisioningStub.RegistrationResponseE;
 import com.swifta.sub.mats.operation.provisioning.v1_0.ProvisioningStub.Registrationrequestresponse;
-import com.swifta.sub.mats.operation.provisioning.v1_0.ProvisioningStub.SecondaryContactInfo;
 import com.swifta.sub.mats.operation.provisioning.v1_0.ProvisioningStub.Securityquestions;
 
 public class UserManagementService {
-	ProvisioningStub provision;
+	ProvisioningStub matsStub;
 
-	@SuppressWarnings("null")
-	public Operationresponse CreateProfile(Address address, int countryID,
-			Date DOB, String employer, String firstname,
-			Identification identification, int gender, int lang,
-			String lastname, int lga, String middlename, String occupation,
-			String prefix, SecondaryContactInfo Scontact,
-			PrimaryContactInfo Pcontact, int stateID, String suffix,
-			String bankdomainname, int bankcodeID, String bankAccount,
-			String clearingNumber, String currency, String email,
-			String msisdn, int profileID, Securityquestions[] secQuestion,
-			String TermsCondition, String username) throws RemoteException,
-			AxisFault {
-		provision = new ProvisioningStub();
+	public String registerProfile(Accountholderdetails accountholderdetails,
+			String bankAccount, int bankCodeid, String bankdomainNameid,
+			String clearingNumber, String currencyid, String email,
+			String msisdn, int profileid, String securityQuest,
+			String securityAns, String termscondition, String username)
+			throws RemoteException {
+		matsStub = new ProvisioningStub();
+		Registration registration = new Registration();
 
-		Calendar cal = utils.DateToCalendar(DOB);
+		String statusMessage = "";
 
-		RegistrationE registration36 = new RegistrationE();
-		Registration registrationDetails = null;
-		Accountholderdetails AccountHolderDetails = null;
-		AccountHolderDetails.setAddress(address);
-		AccountHolderDetails.setCountryid(countryID);
-		AccountHolderDetails.setDateofbirth(cal);
-		AccountHolderDetails.setEmployer(employer);
-		AccountHolderDetails.setFirstname(firstname);
-		AccountHolderDetails.setGenderid(gender);
-		AccountHolderDetails.setIdentification(identification);
-		AccountHolderDetails.setLanguageid(lang);
-		AccountHolderDetails.setLastname(lastname);
-		AccountHolderDetails.setLgaid(lga);
-		AccountHolderDetails.setMiddlename(middlename);
-		AccountHolderDetails.setOccupation(occupation);
-		AccountHolderDetails.setPrefix(prefix);
-		AccountHolderDetails.setPrimarycontact(Pcontact);
-		AccountHolderDetails.setSecondarycontact(Scontact);
-		AccountHolderDetails.setStateid(stateID);
-		AccountHolderDetails.setSuffix(suffix);
+		registration.setAccountholderdetails(accountholderdetails);
+		registration.setBankaccount(bankAccount);
+		registration.setBankcodeid(bankCodeid);
+		registration.setBankdomainnameid(bankdomainNameid);
+		registration.setClearingnumber(clearingNumber);
+		registration.setCurrencyid(currencyid);
+		registration.setEmail(email);
+		registration.setMsisdn(msisdn);
+		registration.setProfileid(profileid);
 
-		registrationDetails.setAccountholderdetails(AccountHolderDetails);
-		registrationDetails.setBankaccount(bankAccount);
-		registrationDetails.setBankcodeid(bankcodeID);
-		registrationDetails.setBankdomainnameid(bankdomainname);
-		registrationDetails.setCurrencyid(currency);
-		registrationDetails.setClearingnumber(clearingNumber);
-		registrationDetails.setEmail(email);
-		registrationDetails.setMsisdn(msisdn);
-		registrationDetails.setProfileid(profileID);
-		registrationDetails.setSecurityquestions(secQuestion);
-		registrationDetails.setTermscondition(TermsCondition);
-		registrationDetails.setUsername(username);
+		Securityquestions[] sec = new Securityquestions[1];
+		Securityquestions securityQuestion = new Securityquestions();
+		securityQuestion.setAnswer(securityAns);
+		securityQuestion.setQuestion(securityQuest);
+		sec[0] = securityQuestion;
 
-		registration36.setRegistration(registrationDetails);
-		Registrationrequestresponse response = provision
-				.registration(registration36).getRegistrationResponse()
-				.get_return();
-		return response;
+		registration.setSecurityquestions(sec);
+		registration.setTermscondition(termscondition);
+		registration.setUsername(username);
+
+		RegistrationE registrationE = new RegistrationE();
+		registrationE.setRegistration(registration);
+		RegistrationResponseE response = matsStub.registration(registrationE);
+		if (response != null) {
+			RegistrationResponse response2 = response.getRegistrationResponse();
+			if (response2 != null) {
+				Registrationrequestresponse response3 = response2.get_return();
+				statusMessage = response3.getResponsemessage();
+
+			} else {
+				statusMessage = "Response is empty";
+			}
+		} else {
+			statusMessage = "Response is empty";
+		}
+
+		return statusMessage;
 
 	}
 
