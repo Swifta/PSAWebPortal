3e5148cbdbd48458b31bcfbea3e215bfdd278a64
LivePwndz <pwndz172@gmail.com>
2014-11-14 16:16:07 +0300

    Added operations Activate, SetParent, SetDefault Account, and Link User. Linking to the backend too is done.

--- WebContent/VAADIN/widgetsets/com.swifta.mats.web.widgetset.Mats_web_portalWidgetset/deferredjs/DB2755C97E317451E6C68C3235F3C554/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_swifta_mats_web_widgetset_Mats_web_portalWidgetset.runAsyncCallback2('xcb(1427,1,aAd);_.vc=function iec(){V2b((!O2b&&(O2b=new $2b),O2b),this.b.e)};Bsd(ai)(2);\n//# sourceURL=com.swifta.mats.web.widgetset.Mats_web_portalWidgetset-2.js\n')
