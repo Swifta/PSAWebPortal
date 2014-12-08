ada715a7afbaa22b1a9650476312ab28756c55ae
LivePwndz <pwndz172@gmail.com>
2014-12-08 16:39:46 +0300

    More unification of the UI.

--- src/com/swifta/mats/web/usermanagement/MenuItemClickable.java
+++ src/com/swifta/mats/web/usermanagement/MenuItemClickable.java
@@ -6,21 +6,20 @@ import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 
-
-
 public class MenuItemClickable extends HorizontalLayout {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -8462407485011099214L;
 	Label lb;
-	public MenuItemClickable(String caption, String role){
+
+	public MenuItemClickable(String caption, String role) {
 		final String curRole = role;
 		lb = new Label(caption);
 		lb.setStyleName("label_menu_item");
 		addComponent(lb);
 		setSizeFull();
-		super.addLayoutClickListener(new LayoutClickListener(){
+		super.addLayoutClickListener(new LayoutClickListener() {
 			/**
 			 * 
 			 */
@@ -28,27 +27,28 @@ public class MenuItemClickable extends HorizontalLayout {
 
 			@Override
 			public void layoutClick(LayoutClickEvent event) {
-				if(event.getChildComponent() == lb){
-					
-					
-					if(curRole == "add_user"){
-						UI.getCurrent().getSession().setAttribute(WorkSpaceManageUser.SESSION_WORK_AREA, WorkSpaceManageUser.SESSION_VAR_WORK_AREA_ADD_USER);
-						UI.getCurrent().getNavigator().navigateTo(WorkSpaceManageUser.WORK_AREA+"/"+curRole);
-						
-						
+				if (event.getChildComponent() == lb) {
+
+					if (curRole == "add_user") {
+						UI.getCurrent()
+								.getSession()
+								.setAttribute(
+										WorkSpaceManageUser.SESSION_WORK_AREA,
+										WorkSpaceManageUser.SESSION_VAR_WORK_AREA_ADD_USER);
+						UI.getCurrent()
+								.getNavigator()
+								.navigateTo(
+										WorkSpaceManageUser.WORK_AREA + "/"
+												+ curRole);
+
 					}
-					
-					if(curRole == "manage_user"){
-						UI.getCurrent().getSession().setAttribute(WorkSpaceManageUser.SESSION_WORK_AREA, WorkSpaceManageUser.SESSION_VAR_WORK_AREA_MANAGE_USER);
-						UI.getCurrent().getSession().setAttribute(ManageUserModule.SESSION_UMANAGE,ManageUserModule.SESSION_VAR_UMANAGE_SEARCH);
-						UI.getCurrent().getNavigator().navigateTo(WorkSpaceManageUser.WORK_AREA+"/"+curRole);
-					
+
+					if (curRole == "manage_user") {
 					}
 				}
 			}
-			
+
 		});
 	}
-	
-	
+
 }
