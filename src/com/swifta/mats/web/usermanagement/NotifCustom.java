f849e5cb068a923120b95c7cff45249ea2a21d26
LivePwndz <pwndz172@gmail.com>
2014-12-23 13:19:57 +0300

    Extra file for notif.

---
+++ src/com/swifta/mats/web/usermanagement/NotifCustom.java
@@ -0,0 +1,31 @@
+package com.swifta.mats.web.usermanagement;
+
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+public class NotifCustom {
+
+	public static void show(String action, String msg) {
+		create(action, msg);
+	}
+
+	private static void create(String action, String msg) {
+
+		Window pop = new Window(action + " status");
+		VerticalLayout cMessage = new VerticalLayout();
+		cMessage.setMargin(true);
+		Label lb = new Label(msg);
+		cMessage.addComponent(lb);
+		cMessage.setStyleName("c_notif");
+		cMessage.setComponentAlignment(lb, Alignment.MIDDLE_CENTER);
+		pop.setContent(cMessage);
+		UI.getCurrent().addWindow(pop);
+		pop.setResizable(false);
+		pop.setDraggable(false);
+		pop.center();
+	}
+
+}
