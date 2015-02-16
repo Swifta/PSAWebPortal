bd3cecbd811d52473753c5f7f5ffed4877fbd174
oonwuzu <oonwuzu@swifta.com>
2015-02-16 13:43:07 +0100

    now testing

--- src/com/swifta/mats/web/report/Reportform.java
+++ src/com/swifta/mats/web/report/Reportform.java
@@ -647,7 +647,7 @@ public class Reportform extends VerticalLayout {
 							.getItemProperty("Date");
 
 					Property<String> tdPropertyfullname = trItem
-							.getItemProperty("Full Name");
+							.getItemProperty("Dealer Full Name");
 
 					// String agentid = rs.getString("operatorid");
 					String aid = rs.getString("aid");
